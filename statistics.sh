887217264da733089dae71f9432182e51745d447
YangXiuyu <1125920908@qq.com>
2022-04-23 11:00:21 +0800

    update pts_merge

---
+++ statistics.sh
@@ -0,0 +1,1 @@
+git log --format='%aN' | sort -u | while read name; do echo -en "$name\t,"; git log --author="$name" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "新增行数: %s, 移除行数: %s, 总行数: %s\n", add, subs, loc }' -; done
