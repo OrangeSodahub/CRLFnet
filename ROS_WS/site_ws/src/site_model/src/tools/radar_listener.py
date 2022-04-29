f98e304860b6aa592a1b1237cbb611581061e46d
YangXiuyu <1125920908@qq.com>
2022-04-29 21:38:51 +0800

    modify radar_listener

--- ROS_WS/site_ws/src/site_model/src/tools/radar_listener.py
+++ ROS_WS/site_ws/src/site_model/src/tools/radar_listener.py
@@ -6,57 +6,44 @@
 
 from sqlalchemy import true
 import rospy
-from per_msgs.msg._SensorMsgsRadar import * # Radar message class
+from per_msgs.msg._SensorMsgsRadar import *
 from radar_msgs.msg._MsgRadar import *
 from radar_msgs.msg._MsgObject import *
+import message_filters
 import std_msgs.msg
 
-msgradar = MsgRadar()
-
-def radar_listener():
-    rospy.init_node("radar_listener", anonymous=true)
-    print("Radar Listener Begin.")
-
-    sub2 = rospy.Subscriber("/ARS_408_21_2_Topic", SensorMsgsRadar, msgs_publish_2)
-    sub3 = rospy.Subscriber("/ARS_408_21_3_Topic", SensorMsgsRadar, msgs_publish_3)
-    rospy.spin()    
-
-def msgs_publish_2(msgs: SensorMsgsRadar):
-    """
-        Receive per_msgs::SensorMsgsRadar continuously
-        Publish radar_msgs::MsgRadar at a rate of 10Hz
-    """
-    if msgs.total_front_left_esr_tracks != 0: # if num of vehicles is not 0
-        msgradar.total_vehicles_left = round(msgs.total_front_left_esr_tracks)
-        msgradar.ObjectList_left.clear() # every time write msgs clear lists first
+def radar_listener(radar2: SensorMsgsRadar, radar3: SensorMsgsRadar):
+    msgradar = MsgRadar()
+    if radar2.total_front_left_esr_tracks != 0: # if num of vehicles is not 0
+        msgradar.total_vehicles_left = round(radar2.total_front_left_esr_tracks)
         for i in range(msgradar.total_vehicles_left):
            object = MsgObject() # define a new list
-           object.obj_vcs_posex = msgs.front_left_esr_tracklist[i].obj_vcs_posex
-           object.obj_vcs_posey = msgs.front_left_esr_tracklist[i].obj_vcs_posey
-           object.range_rate = msgs.front_left_esr_tracklist[i].range_rate
+           object.obj_vcs_posex = radar2.front_left_esr_tracklist[i].obj_vcs_posex
+           object.obj_vcs_posey = radar2.front_left_esr_tracklist[i].obj_vcs_posey
+           object.range_rate = radar2.front_left_esr_tracklist[i].range_rate
            msgradar.ObjectList_left.append(object)
 
-def msgs_publish_3(msgs: SensorMsgsRadar):
-    if msgs.total_front_right_esr_tracks !=0:
-        msgradar.total_vehicles_right = round(msgs.total_front_right_esr_tracks)
-        msgradar.ObjectList_right.clear()
+    if radar3.total_front_right_esr_tracks !=0:
+        msgradar.total_vehicles_right = round(radar3.total_front_right_esr_tracks)
         for i in range(msgradar.total_vehicles_right):
            object = MsgObject()
-           object.obj_vcs_posex = msgs.front_right_esr_tracklist[i].obj_vcs_posex
-           object.obj_vcs_posey = msgs.front_right_esr_tracklist[i].obj_vcs_posey
-           object.range_rate = msgs.front_right_esr_tracklist[i].range_rate
+           object.obj_vcs_posex = radar3.front_right_esr_tracklist[i].obj_vcs_posex
+           object.obj_vcs_posey = radar3.front_right_esr_tracklist[i].obj_vcs_posey
+           object.range_rate = radar3.front_right_esr_tracklist[i].range_rate
            msgradar.ObjectList_right.append(object)
 
     pub = rospy.Publisher("/radar_msgs_combined", MsgRadar) # MsgRadar is the defined message type
-    # rate = rospy.Rate(10) # publish msgs rated at 10Hz
-    MsgRadar.header = std_msgs.msg.Header()
-    MsgRadar.header.stamp = rospy.Time.now() # Add time stamp
+    msgradar.header.stamp = rospy.Time.now() # Add time stamp
     pub.publish(msgradar)
-    # rate.sleep()
-
 
 if __name__ == '__main__':
-    try:
-        radar_listener()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node("radar_listener", anonymous=true)
+
+    sub_radar_2 = message_filters.Subscriber("/ARS_408_21_2_Topic", SensorMsgsRadar)
+    sub_radar_3 = message_filters.Subscriber("/ARS_408_21_3_Topic", SensorMsgsRadar)
+
+    sync = message_filters.ApproximateTimeSynchronizer([sub_radar_2, sub_radar_3], 1, 1)
+
+    sync.registerCallback(radar_listener)
+    print("Radar Listener Begin.")
+    rospy.spin()
