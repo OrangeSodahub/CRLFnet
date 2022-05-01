bb31147cf646c3de527cd652fc7949d0230fa180
YangXiuyu <1125920908@qq.com>
2022-05-02 00:58:09 +0800

    update custom_dataset

--- src/site_model/src/LidCamFusion/OpenPCDet/pcdet/datasets/custom/custom_dataset.py
+++ src/site_model/src/LidCamFusion/OpenPCDet/pcdet/datasets/custom/custom_dataset.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import copy
+from multiprocessing import allow_connection_pickling
 import pickle
 import os
 
@@ -31,6 +33,8 @@ class CustomDataset(DatasetTemplate):
         print("split_dir:",split_dir)
         self.sample_id_list = [x.strip() for x in open(split_dir).readlines()] if os.path.exists(split_dir) else None
 
+        self.custom_infos = []
+        self.include_custom_data(self.mode)
         self.ext = ext
 
                 
@@ -73,6 +77,24 @@ class CustomDataset(DatasetTemplate):
 
         data_dict = self.prepare_data(data_dict=input_dict)
         return data_dict
+
+    def include_custom_data(self, mode):
+        if self.logger is not None:
+            self.logger.info('Loading Custom dataset.')
+        custom_infos = []
+
+        for info_path in self.dataset_cfg.INFO_PATH[mode]:
+            info_path = self.root_path / info_path
+            if not info_path.exists():
+                continue
+            with open(info_path, 'rb') as f:
+                infos = pickle.load(f)
+                custom_infos.extend(infos)
+        
+        self.custom_infos.extend(custom_infos)
+
+        if self.logger is not None:
+            self.logger.info('Total samples for CUSTOM dataset: %d' % (len(custom_infos)))
 
     def create_groundtruth_database(self, info_path=None, used_classes=None, split='train'):
             import torch
@@ -125,3 +147,81 @@ class CustomDataset(DatasetTemplate):
 
             with open(db_info_save_path, 'wb') as f:
                 pickle.dump(all_db_infos, f)
+
+    def get_infors(self, num_workers=4, has_label=True, count_inside_pts=True, sample_id_list=None):
+        import concurrent.futures as futures
+
+        def process_single_scene(sample_idx):
+            print('%s sample_idx: %s' % (self.split, sample_idx))
+            # define an empty dict
+            info = {}
+            # pts infos: dimention and inx
+            pc_info = {'num_features': 4, 'lidar_idx': sample_idx}
+            # add to pts infos
+            info['point_cloud'] = pc_info
+
+            # no images neither calibs
+
+            type_to_id = {'Car': 1, 'Pedestrian': 2, 'Cyclist': 3}
+            if has_label:
+                # read labels to build object list according to idx
+                obj_list = self.get_label(sample_idx)
+                # build an empty annotations list
+                annotations = {}
+                # add to annotations ==> refer to 'object3d_kitti' (no truncated,occluded,alpha,bbox)
+                annotations['name'] = np.array([obj[0] for obj in obj_list])
+                annotations['dimensions'] = np.array([[float(obj[10]), float(obj[8]), float(obj[9])] for obj in obj_list]) # lhw(camera) format
+                annotations['location'] = np.concatenate([np.array((float(obj[11]), float(obj[12]), float(obj[13])), dtype=np.float32).reshape(1,3) for obj in obj_list], axis=0)
+                annotations['rotation_y'] = np.array([float(obj[14]) for obj in obj_list])
+
+                num_objects = len([obj[0] for obj in obj_list])
+                num_gt = len(annotations['name'])
+                index = list(range(num_objects)) + [-1] * (num_gt - num_objects)
+                annotations['index'] = np.array(index, dtype=np.int32)
+
+                # attention 'rots'
+                loc = annotations['location'][:num_objects]
+                dims = annotations['dimensions'][:num_objects]
+                rots = annotations['rotation_y'][:num_objects] # * (np.pi / 100)
+                l, h, w = dims[:, 0:1], dims[:, 1:2], dims[:, 2:3]
+                gt_boxes_lidar = np.concatenate([annotations['location'], l, w, h, rots], axis=1 )
+                annotations['gt_boxes_lidar'] = gt_boxes_lidar
+                print(annotations)
+                
+                # add annotation info
+                info['annos'] = annotations
+            
+            return info
+        
+        sample_id_list = sample_id_list if sample_id_list is not None else self.sample_id_list
+        # create a thread pool to improve the velocity
+        with futures.ThreadPoolExecutor(num_workers) as executor:
+            infos = executor.map(process_single_scene, sample_id_list)
+        # infos is a list that each element represents per frame
+        return list(infos)
+                
+
+    def get_label(self, idx):
+        # get labels
+        label_file = self.root_split_path / 'label_2' / ('%s.txt' % idx)
+        # 'object3d.kitti.get_objects_from_label'
+        assert label_file.exists()
+        objects = []
+        with open(label_file, 'r') as f:
+            for i in f.readlines():
+                cur_object = i.strip('\n').split(' ')
+                objects.append(cur_object)
+        return objects
+
+    def get_lidar(self, idx):
+        """
+            Loads point clouds for a sample
+                Args:
+                    index (int): Index of the point cloud file to get.
+                Returns:
+                    np.array(N, 4): point cloud.
+        """
+        # get lidar statistics
+        lidar_file = self.root_split_path / 'velodyne' / ('%s.bin' % idx)
+        assert lidar_file.exists()
+        return np.fromfile(str(lidar_file), dtype=np.float32).reshape(-1, 4)
