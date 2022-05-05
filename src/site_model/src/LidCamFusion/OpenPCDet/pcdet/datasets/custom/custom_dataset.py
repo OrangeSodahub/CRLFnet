ae994b1572e0050c0a458ac5c33284d7fdd8fdea
OrangeSodahub <1125920908@qq.com>
2022-05-05 16:32:17 +0800

    Add Prediction

--- src/site_model/src/LidCamFusion/OpenPCDet/pcdet/datasets/custom/custom_dataset.py
+++ src/site_model/src/LidCamFusion/OpenPCDet/pcdet/datasets/custom/custom_dataset.py
@@ -255,6 +255,74 @@ class CustomDataset(DatasetTemplate):
         split_dir = self.root_path / 'ImageSets' / (self.split + '.txt')
         self.sample_id_list = [x.strip() for x in open(split_dir).readlines()] if split_dir.exists() else None
 
+    @staticmethod
+    def generate_prediction_dicts(batch_dict, pred_dicts, class_names, output_path=None):
+        """
+        Args:
+            batch_dict:
+                frame_id:
+            pred_dicts: list of pred_dicts
+                pred_boxes: (N,7), Tensor
+                pred_scores: (N), Tensor
+                pred_lables: (N), Tensor
+            class_names:
+            output_path:
+
+        Returns:
+
+        """
+        def get_template_prediction(num_smaples):
+            ret_dict = {
+                'name': np.zeros(num_smaples), 'alpha' : np.zeros(num_smaples),
+                'dimensions': np.zeros([num_smaples, 3]), 'location': np.zeros([num_smaples, 3]),
+                'rotation_y': np.zero(num_smaples), 'score': np.zeros(num_smaples),
+                'boxes_lidar': np.zeros([num_smaples, 7])
+            }
+            return ret_dict
+
+        def generate_single_sample_dict(batch_index, box_dict):
+            pred_scores = box_dict['pred_scores'].cpu().numpy()
+            pred_boxes = box_dict['pred_boxes'].cpu().numpy()
+            pred_labels = box_dict['pred_labels'].cpu().numpy()
+            pred_dict = get_template_prediction(pred_scores.shape[0])
+            if pred_scores.shape[0] == 0:
+                return pred_dict
+
+            pred_dict['name'] = np.array(class_names)[pred_labels - 1]
+            pred_dict['alpha'] = -np.arctan2(-pred_boxes[:, 1], pred_boxes[:, 0]) + pred_boxes_camera[:, 6]
+            # pred_dict['dimensions'] = pred_boxes_camera[:, 3:6]
+            # pred_dict['location'] = pred_boxes_camera[:, 0:3]
+            # pred_dict['rotation_y'] = pred_boxes_camera[:, 6]
+            pred_dict['score'] = pred_scores
+            pred_dict['boxes_lidar'] = pred_boxes
+
+            return pred_dict
+
+        annos = []
+        for index, box_dict in enumerate(pred_dicts):
+            frame_id = batch_dict['frame_id'][index]
+
+            single_pred_dict = generate_single_sample_dict(index, box_dict)
+            single_pred_dict['frame_id'] = frame_id
+            annos.append(single_pred_dict)
+
+            if output_path is not None:
+                cur_det_file = output_path / ('%s.txt' % frame_id)
+                with open(cur_det_file, 'w') as f:
+                    bbox = single_pred_dict['bbox']
+                    loc = single_pred_dict['location']
+                    dims = single_pred_dict['dimensions']  # lhw -> hwl
+
+                    for idx in range(len(bbox)):
+                        print('%s -1 -1 %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f'
+                            % (single_pred_dict['name'][idx], single_pred_dict['alpha'][idx],
+                                bbox[idx][0], bbox[idx][1], bbox[idx][2], bbox[idx][3],
+                                dims[idx][1], dims[idx][2], dims[idx][0], loc[idx][0],
+                                loc[idx][1], loc[idx][2], single_pred_dict['rotation_y'][idx],
+                                single_pred_dict['score'][idx]), file=f)
+            return annos
+
+
 def create_custom_infos(dataset_cfg, class_names, data_path, save_path, workers=4):
     dataset = CustomDataset(dataset_cfg=dataset_cfg, class_names=class_names, root_path=data_path, training=False)
     train_split, val_split = 'train', 'val'
