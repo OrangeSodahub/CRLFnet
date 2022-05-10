d9f5b0b3e6742a270a973e048e9f8d0cca151f88
YangXiuyu <1125920908@qq.com>
2022-05-10 16:00:35 +0800

    add new

--- src/site_model/src/LidCamFusion/OpenPCDet/tools/demo.py
+++ src/site_model/src/LidCamFusion/OpenPCDet/tools/demo.py
@@ -14,6 +14,8 @@ except:
 import numpy as np
 import torch
 
+import sys
+sys.path.append('/home/zonlin/CRLFnet/src/site_model/src/LidCamFusion/OpenPCDet/')
 from pcdet.config import cfg, cfg_from_yaml_file
 from pcdet.datasets import DatasetTemplate
 from pcdet.models import build_network, load_data_to_gpu
