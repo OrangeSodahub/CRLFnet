94aebe371ed3a53b53fb4a41b1e4dabad25df085
YangXiuyu <gzzyyxy@gmail.com>
2022-06-29 12:56:45 +0800

    add LidCamFusion fps evaluation

--- src/site_model/src/LidCamFusion/fusion.py
+++ src/site_model/src/LidCamFusion/fusion.py
@@ -11,11 +11,14 @@ import yaml
 import rospy
 from termcolor import colored
 import message_filters
+import tensorboard
 # pointcloud type
 from sensor_msgs.msg import PointCloud2
 import ros_numpy
 # Image type
 from msgs.msg._MsgCamera import * # camera msgs class
+# odometry type
+from nav_msgs.msg import Odometry
 # Object Detection tool
 from OpenPCDet.tools.pred import *
 # pointcloud detection
@@ -30,7 +33,8 @@ from tools.RadCamFusion import image_roi
 # visualization
 from utils import visualization
 
-def fusion(pointcloud, msgcamera):
+
+def fusion(pointcloud, msgcamera, odom=None):
     """
         pointcloud: [N,4]
         msgcamera: [1,8] -> eight Images
@@ -44,11 +48,7 @@ def fusion(pointcloud, msgcamera):
         pred_boxes2d.append(image_roi.image_roi(img, yolo))
 
     # pointcloud roi
-    pc = ros_numpy.numpify(pointcloud)
-    points = np.zeros((pc.shape[0],4))
-    points[:,0] = pc['x']
-    points[:,1] = pc['y']
-    points[:,2] = pc['z']
+    points = convert_ros_pointcloud_to_numpy(pointcloud)
     pred_boxes3d, pred_labels, pred_scores = pointcloud_detector.get_pred_dicts(points, False)
 
     # label2calss
@@ -58,7 +58,13 @@ def fusion(pointcloud, msgcamera):
         3: 'Bicycle'
     }
 
+    # post process the predict results
     if len(pred_boxes3d) != 0:
+
+        # pred results eval
+        
+
+        
         if params.print2screen:
             print("+-------------------------------------------------------------------------------------------+")
             print("num_car: ", len(pred_boxes3d))
@@ -75,13 +81,32 @@ def fusion(pointcloud, msgcamera):
             output_dir = ROOT_DIR + config['output']['LidCamFusion_dir']
             visualization.lidar2visual(cameras, pixel_poses, msgcamera, output_dir)
 
-    # fusion
-    msglidcam = MsgLidCam()
-    msglidcam.header.stamp = rospy.Time.now()
+    # # fusion
+    # msglidcam = MsgLidCam()
+    # msglidcam.header.stamp = rospy.Time.now()
 
-    publish result
-    pub = rospy.Publisher("/lidar_camera_fused", MsgLidCam)
-    pub.publish(msglidcam)
+    # # publish result
+    # pub = rospy.Publisher("/lidar_camera_fused", MsgLidCam)
+    # pub.publish(msglidcam)
+
+    # fps evalution (without results evalution and visualization)
+    global time_span, counter
+    cur_time = time.time()
+    time_span += cur_time - start_time
+    start_time = cur_time
+    counter += 1
+    fps = counter / (time_span*1e6)
+    
+
+def convert_ros_pointcloud_to_numpy(pointcloud: PointCloud2):
+    pc = ros_numpy.numpify(pointcloud)
+    points = np.zeros((pc.shape[0],4))
+    points[:,0] = pc['x']
+    points[:,1] = pc['y']
+    points[:,2] = pc['z']
+
+    return points
+
 
 if __name__ == '__main__':
     # get root path
@@ -92,6 +117,7 @@ if __name__ == '__main__':
     parser.add_argument("--config", help="path to config file", metavar="FILE", required=False, default= ROOT_DIR + '/config/config.yaml')
     parser.add_argument("--draw_output", help="wehter to draw rois and output", action='store_true', required=False)
     parser.add_argument("--print2screen", help="wehter to print to screen", action='store_true', required=False)
+    parser.add_argument("--eval", help="wehter to eval", action='store_true', required=False)
     params = parser.parse_args()
 
     with open(params.config, 'r') as f:
@@ -103,6 +129,11 @@ if __name__ == '__main__':
 
     rospy.init_node('lidar_camera_fusion', anonymous=True)
 
+    # fps evaluation
+    counter = 0
+    time_span = 0
+    fps = 0
+
     # Create an example of pointcloud detector
     pointcloud_detector = RT_Pred(ROOT_DIR, config)
     # Create YOLO detector
@@ -110,8 +141,17 @@ if __name__ == '__main__':
 
     sub_pointcloud = message_filters.Subscriber('/point_cloud_combined', PointCloud2)
     sub_camera = message_filters.Subscriber('/camera_msgs_combined', MsgCamera)
- 
-    sync = message_filters.ApproximateTimeSynchronizer([sub_pointcloud, sub_camera], 1, 1)# syncronize time stamps
-    sync.registerCallback(fusion)
-    print("Lidar Camera Fusion Begin.")
-    rospy.spin()
+
+    if params.eval:
+        sub_odom = message_filters.SimpleFilter('//base_pose_ground_truth', )
+        sync = message_filters.ApproximateTimeSynchronizer([sub_pointcloud, sub_camera, sub_odom], 1, 1) # syncronize time stamps
+        sync.registerCallback(fusion)
+        print("Lidar Camera Fusion (with eval) Begin.")
+        start_time = time.time()
+        rospy.spin()
+    else:
+        sync = message_filters.ApproximateTimeSynchronizer([sub_pointcloud, sub_camera], 1, 1)
+        sync.registerCallback(fusion)
+        print("Lidar Camera Fusion Begin.")
+        start_time = time.time()
+        rospy.spin()
--- src/site_model/src/utils/test.py
+++ src/site_model/src/utils/test.py
@@ -1,6 +1,2 @@
-from pathlib import Path
+# test for transformation or coordinates
 
-if __name__=='__main__':
-    # ROOT_DIR = (Path(__file__).resolve().parent / '../../../').resolve()
-    ROOT_DIR = (Path(__file__).resolve().parent)
-    print(str(ROOT_DIR) + '/fuck/')
