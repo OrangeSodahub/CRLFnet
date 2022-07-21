da5fde1496116652567472e959c1ee4c10ac99ce
zzy <2657956890@qq.com>
2022-07-21 16:39:19 +0800

    Kalman finished, but still bugs

--- src/site_model/src/utils/kalman.py
+++ src/site_model/src/utils/kalman.py
@@ -5,6 +5,8 @@ Kalman Filter
 """
 
 
+from tkinter import OFF
+from cv2 import norm
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
@@ -14,9 +16,8 @@ from .transform import w2p
 _Q_DEFAULT = 1e-2 * np.eye(4)
 
 _THRESHOLD = 1e4
-_WEIGHT_MATCH = 1
-_WEIGHT_RADAR = 1
-_WEIGHT_IMAGE = 1
+_WEIGHT_RADAR = 0.7
+_WEIGHT_IMAGE = 0.3
 
 OFFSET = np.array([-1.374, -1.176])
 HEIGHT = 0.461
@@ -57,16 +58,42 @@ def _single_update(pred_xpt: np.ndarray, pred_cov: np.ndarray, z: np.ndarray, H,
     return xpt, cov
 
 
-def _single_comparison(pred_xpt: np.ndarray, z: np.ndarray, H: np.ndarray):
-    zp = np.matmul(H, pred_xpt)
-    print("\033[0;31mcmp\033[0m", z, zp)
+def _single_radar_comparison(pred_xpt: np.ndarray, z: np.ndarray, offset: np.ndarray):
+    x, y, vx, vy = pred_xpt[0] - offset[0], pred_xpt[1] - offset[1], pred_xpt[2], pred_xpt[3]
+    r = np.sqrt(np.square(x) + np.square(y))
+    if x == 0:
+        theta = 0
+    elif y == 0 and x < 0:
+        theta = 90
+    elif y == 0 and x > 0:
+        theta = -90
+    else:
+        theta = -np.arctan(x / y)
+        if x < 0 and y < 0:
+            theta = 180 - theta
+        elif x > 0 and y < 0:
+            theta = -180 + theta
+    vn = (vy * y + vx * x) / r
+    zp = np.array([r, theta, vn])
+    print("\033[0;31mCMP, Radar:\033[0m", z, zp)
     d = np.linalg.norm(z - zp)
     return d
 
 
-def _get_comparison_matrix(xpts: np.ndarray, zs: np.ndarray, H_func):
-    cmp = [_single_comparison(x, z, H_func(x)) for z in zs for x in xpts]
-    return np.array(cmp).reshape((len(xpts), len(zs)))
+def _single_image_comparison(pred_xpt: np.ndarray, z: np.ndarray, w2c, c2p):
+    pos = np.array([pred_xpt[0], pred_xpt[1], HEIGHT, 1])
+    zp, _ = w2p(pos, w2c, c2p)
+    zp = zp[0:2]
+    print("\033[0;31mCMP, Image:\033[0m", z, zp)
+    d = np.linalg.norm(z - zp)
+    return d
+
+
+def _single_match_comparison(pred_xpt: np.ndarray, z: np.ndarray, offset: np.ndarray, w2c, c2p):
+    d1 = _single_radar_comparison(pred_xpt, z[0:3], offset)
+    d2 = _single_image_comparison(pred_xpt, z[3:5], w2c, c2p)
+    d = d1 * _WEIGHT_RADAR + d2 * _WEIGHT_IMAGE
+    return d
 
 
 def bulk_predict(prev_xpt_pile: list, prev_cov_pile: list, A, Q=_Q_DEFAULT):
@@ -74,27 +101,27 @@ def bulk_predict(prev_xpt_pile: list, prev_cov_pile: list, A, Q=_Q_DEFAULT):
     return [xpt for xpt, _ in p0], [cov for _, cov in p0]
 
 
-def bulk_comparison(pred_xpt_pile, zms, zrs, zis, w2c, c2p):
-    # row -> z, col -> xpt
+def bulk_comparison(pred_xpt_pile, zms, zrs, zis, w2c, c2p, offset=OFFSET):
+    # row -> xpts, col -> zs
     xpt_num = len(pred_xpt_pile)
     z_num = len(zms) + len(zrs) + len(zis)
-    cmp_match = _get_comparison_matrix(pred_xpt_pile, zms, lambda x: get_H_match(x, w2c, c2p))
-    cmp_radar = _get_comparison_matrix(pred_xpt_pile, zrs, lambda x: get_H_radar(x, OFFSET))
-    cmp_image = _get_comparison_matrix(pred_xpt_pile, zis, lambda x: get_H_image(x, w2c, c2p))
+    cmp_match = np.array([_single_match_comparison(x, z, offset, w2c, c2p) for x in pred_xpt_pile for z in zms]).reshape(xpt_num, len(zms))
+    cmp_radar = np.array([_single_radar_comparison(x, z, offset) for x in pred_xpt_pile for z in zrs]).reshape(xpt_num, len(zrs))
+    cmp_image = np.array([_single_image_comparison(x, z, w2c, c2p) for x in pred_xpt_pile for z in zis]).reshape(xpt_num, len(zis))
     cmp = np.concatenate((cmp_match, cmp_radar, cmp_image), axis=1)
     print("Bulk Comparison:", cmp)
     row_idx, col_idx = linear_sum_assignment(cmp)
     # threshold filter
     fil = cmp[row_idx, col_idx] < _THRESHOLD
     row_idx = np.extract(fil, row_idx)
-    new_idx = np.setdiff1d(np.arange(z_num), row_idx)
+    lost_idx = np.setdiff1d(np.arange(xpt_num), row_idx)
     col_idx = np.extract(fil, col_idx)
-    lost_idx = np.setdiff1d(np.arange(xpt_num), col_idx)
+    new_idx = np.setdiff1d(np.arange(z_num), col_idx)
     # existing objs, lost objs, new objs
     return (row_idx, col_idx), lost_idx, new_idx
 
 
-def bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_cov_pile, zms, zrs, zis, w2c, c2p):
+def bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_cov_pile, zms, zrs, zis, w2c, c2p, offset=OFFSET):
     for pred_idx, z_idx in zip(exist_idx[0], exist_idx[1]):
         if z_idx < len(zms):
             # match
@@ -103,7 +130,7 @@ def bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_c
             R = 0.1 * np.eye(5)
         elif z_idx < len(zms) + len(zrs):
             # radar
-            H = get_H_radar(pred_xpt_pile[pred_idx], OFFSET)
+            H = get_H_radar(pred_xpt_pile[pred_idx], offset)
             z = zrs[z_idx - len(zms)]
             R = 0.1 * np.eye(3)
         else:
@@ -112,7 +139,10 @@ def bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_c
             z = zis[z_idx - len(zms) - len(zrs)]
             R = 0.1 * np.eye(2)
         xpt, cov = _single_update(pred_xpt_pile[pred_idx], pred_cov_pile[pred_idx], z, H, R)
-        state_xpt_pile[pred_idx] = np.concatenate((xpt, [state_xpt_pile[pred_idx][4] + 1, state_xpt_pile[pred_idx][5]])) 
+        age = state_xpt_pile[pred_idx][4] + 1
+        if age > 3:
+            age = 3
+        state_xpt_pile[pred_idx] = np.concatenate((xpt, [age, state_xpt_pile[pred_idx][5]])) 
         state_cov_pile[pred_idx] = cov
 
 
