f9f81c634caf8b51bec0534fd4953fe5c7cdd6ec
zzy <2657956890@qq.com>
2022-07-22 15:03:31 +0800

    Rewrite Kalman filter

--- src/site_model/src/RadCamFusion/fusion.py
+++ src/site_model/src/RadCamFusion/fusion.py
@@ -11,18 +11,15 @@ import argparse
 import yaml
 import numpy as np
 
-import cv2
-from cv_bridge import CvBridge
-
 import rospy
 import message_filters
 from sensor_msgs.msg import Image               # Camera message
 from msgs.msg._MsgRadar import MsgRadar         # Radar message
 from msgs.msg._MsgRadCam import MsgRadCam       # fusion message
 
-from ..utils.poi_and_roi import radar_poi, image_roi, expand_poi, optimize_iou
-from ..utils.kalman import *
 from ..utils.yolo.yolo import YOLO
+from ..utils.kalman import KalmanFilter
+from ..utils.poi_and_roi import radar_poi, image_roi, expand_poi, optimize_iou
 from ..utils.visualization import radar2visual  # visualized output
 
 
@@ -32,20 +29,8 @@ def residual(uir, uii):
     return ri, ii
 
 
-def pair_fusion(radar: MsgRadar, image: Image, w2c: np.ndarray, c2p: np.ndarray, name: str):
-    # ONLY FUSE THE LEFT PAIR!!!
+def pair_fusion(radar: MsgRadar, image: Image, kf: KalmanFilter, w2c: np.ndarray, c2p: np.ndarray, del_time: float):
     global yolo
-    global state_xpt_pile, state_cov_pile
-    global w2cs, c2ps
-    global frame_counter, my_timer
-    global max_id
-
-    # Info
-    my_now = perf_counter()
-    del_time = my_now - my_timer
-    print("\033[1;36m\"{}\", FPS: {:.2f}, Frame {}\033[0m".format(name, 1.0 / del_time, frame_counter))
-    frame_counter += 1
-    my_timer = my_now
 
     # Separate Decection
     print("\033[1;36mSeparate Detection\033[0m")
@@ -73,11 +58,11 @@ def pair_fusion(radar: MsgRadar, image: Image, w2c: np.ndarray, c2p: np.ndarray,
                                             radar_pois, radar_zs[:, 0])),
                                             dtype=int)
     print("Radar expanded ROIs:\t", radar_expanded_rois)
-    match_pairs = optimize_iou(radar_expanded_rois, image_rois)
+    match_pairs = optimize_iou(radar_expanded_rois, image_rois, threshold=1.0)
     print("Matched Idx (r/i):  \t", match_pairs)
     # residual algorithm (NOT finished)
-    radar_indices, image_indices = (np.setdiff1d(np.arange(radar_obj_num), match_pairs[0]),
-                                    np.setdiff1d(np.arange(image_obj_num), match_pairs[1]))
+    radar_indices = np.setdiff1d(np.arange(radar_obj_num), match_pairs[0])
+    image_indices = np.setdiff1d(np.arange(image_obj_num), match_pairs[1])
     print("Unmatched Radar Idx:\t", radar_indices)
     print("Unmatched Image Idx:\t", image_indices)
     """
@@ -89,41 +74,40 @@ def pair_fusion(radar: MsgRadar, image: Image, w2c: np.ndarray, c2p: np.ndarray,
     zrs = radar_zs[radar_indices, 0:3]
     zis = image_zs[image_indices, 0:2]
     # EKF
-    # state_xpt = (Xw, Yw, Vx, Vy, age, id)
     A = np.array([[1, 0, del_time, 0], [0, 1, 0, del_time], [0, 0, 1, 0], [0, 0, 0, 1]])
-    pred_xpt_pile, pred_cov_pile = bulk_predict(state_xpt_pile, state_cov_pile, A)
-    print("Predicted States:   \t", pred_xpt_pile)
-    exist_idx, lost_idx, new_idx = bulk_comparison(pred_xpt_pile, zms, zrs, zis, w2c, c2p)
-    print("Exist Idx (s/o):    \t", exist_idx)
-    print("Lost Idx  (s):      \t", lost_idx)
-    print("New Idx   (obs):    \t", new_idx)
-    bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_cov_pile, zms, zrs, zis, w2c, c2p)
-    bulk_lost(lost_idx, state_xpt_pile, state_cov_pile)
-    max_id = bulk_new(new_idx, state_xpt_pile, state_cov_pile, zms, zrs, zis, max_id)
+    Q = np.eye(4, 5e-2)
+    kf.all_in_one()
+    kf.output()
 
     # Save Images
     if args.save and radar_obj_num !=0 and image_obj_num != 0:
         radar2visual(OUTPUT_DIR, image, radar_pois=radar_pois, radar_rois=radar_expanded_rois, image_rois=image_rois, appendix='Test')
     
     # Publish
-    print("Updated States     :\t", state_xpt_pile)
 
 
 def fusion(radar: MsgRadar, image2: Image, image3: Image):
+    global my_timer, frame_counter
+    global kf2
+    global w2cs, c2ps
+
+    # Output FPS and Frame Info
     print('+------------------------+')
-    pair_fusion(radar, image2, w2cs['camera2'], c2ps['camera2'], 'camera2')
-    # pair_fusion(radar, image3, 'camera3')
+    my_now = perf_counter()
+    del_time = my_now - my_timer
+    print("\033[1;36mFrame {}, FPS: {:.2f}\033[0m".format(frame_counter, 1.0 / del_time))
+    frame_counter += 1
+    my_timer = my_now
+
+    # Pair Fusion
+    print("\033[1;36m{}\033[0m".format('camera2'))
+    pair_fusion(radar, image2, kf2, w2cs['camera2'], c2ps['camera2'], del_time, 'camera2')
 
 
 if __name__ == '__main__':
-    # fps counter
+    # fps counter and timer
     my_timer = 0
     frame_counter = 0
-
-    # storage of Kalman Filter
-    max_id = 0
-    state_xpt_pile = []     # (Xw, Yw, Vxw, Vyw, status, index)
-    state_cov_pile = []     # covariance matrices
 
     # set command arguments
     parser = argparse.ArgumentParser()
@@ -186,6 +170,9 @@ if __name__ == '__main__':
         print("\033[0;33mRunning in off-YOLO mode.\033[0m")
         OUTPUT_DIR = OUTPUT_DIR.joinpath("off_yolo")
         OUTPUT_DIR.mkdir(exist_ok=True)
+
+    # initialize Kalman Filter
+    kf2 = KalmanFilter(w2cs['camera2'], c2ps['camera2'])
     
     # initialize publisher
     pub = rospy.Publisher("/radar_camera_fused", MsgRadCam, queue_size=10)
--- src/site_model/src/utils/kalman.py
+++ src/site_model/src/utils/kalman.py
@@ -5,176 +5,188 @@ Kalman Filter
 """
 
 
-from tkinter import OFF
-from cv2 import norm
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 from .transform import w2p
 
 
-_Q_DEFAULT = 1e-2 * np.eye(4)
+class KalmanFilter:
+    '''
+    Semi-nonlinear Filter
+    '''
 
-_THRESHOLD = 1e4
-_WEIGHT_RADAR = 0.7
-_WEIGHT_IMAGE = 0.3
+    def __init__(self, w2c, c2p, radar_pos=np.array([-1.374, -1.176, 0.461])):
+        # xpt = (Xw, Yw, Vx, Vy, age, id)
+        self.xpt_pile = []
+        self.cov_pile = []
+        self.pred_xpt_pile = []
+        self.pred_cov_pile = []
+        self.total_objects = 0
+        self.max_id = 0
 
-OFFSET = np.array([-1.374, -1.176])
-HEIGHT = 0.461
+        self.radar_pos = radar_pos
+        self.w2c = w2c
+        self.c2p = c2p
 
+        self.MAX_AGE = 2
+        self.THRESHOLD = 1e4
+        self.WEIGHT_RADAR = 0.8
+        self.WEIGHT_IMAGE = 0.2
 
-def get_H_radar(pred_xpt: np.ndarray, offset):
-    r = np.linalg.norm(pred_xpt[0:2] - offset[0:2])
-    c = (pred_xpt[1] - offset[1]) / r
-    s = (pred_xpt[0] - offset[0]) / r
-    vn = pred_xpt[2] * c - pred_xpt[3] * s
-    Hr = np.array([[-s, c, 0, 0],
-                   [-c / r, -s / r, 0, 0],
-                   [(pred_xpt[2] - vn * s) / r, (pred_xpt[3] - vn * c) / r, -s, c]])
-    return Hr
+    def __H_radar(self, pred_xpt: np.ndarray):
+        r = np.linalg.norm(pred_xpt[0:2] - self.radar_pos[0:2])
+        c = (pred_xpt[1] - self.radar_pos[1]) / r
+        s = (pred_xpt[0] - self.radar_pos[0]) / r
+        vn = pred_xpt[2] * c - pred_xpt[3] * s
+        Hr = np.array( [[-s, c, 0, 0],
+                        [-c / r, -s / r, 0, 0],
+                        [(pred_xpt[2] - vn * s) / r, (pred_xpt[3] - vn * c) / r, -s, c]])
+        return Hr
 
+    def __H_image(self, pred_xpt: np.ndarray):
+        uv, zc = w2p(np.array([pred_xpt[0], pred_xpt[1], self.radar_pos[2], 1]), self.w2c, self.c2p)
+        m0 = np.concatenate((np.matmul(self.c2p, self.w2c)[0:2, 0:2], np.zeros(shape=(2, 2))), axis=1)
+        m1 = np.outer(uv[0:2], [self.w2c[2, 0], self.w2c[2, 1], 0, 0])
+        Hi = (m0 - m1) / zc
+        return Hi
 
-def get_H_image(pred_xpt: np.ndarray, w2c, c2p):
-    uv, zc = w2p(np.array([pred_xpt[0], pred_xpt[1], HEIGHT, 1]), w2c, c2p)
-    m0 = np.concatenate((np.matmul(c2p, w2c)[0:2, 0:2], np.zeros(shape=(2, 2))), axis=1)
-    m1 = np.outer(uv[0:2], [w2c[2, 0], w2c[2, 1], 0, 0])
-    Hi = (m0 - m1) / zc
-    return Hi
+    def __H_match(self, pred_xpt: np.ndarray):
+        Hr = self.__H_radar(pred_xpt)
+        Hi = self.__H_image(pred_xpt)
+        Hm = np.concatenate((Hr, Hi))
+        return Hm
 
-
-get_H_match = lambda pred_xpt, w2c, c2p: np.concatenate((get_H_radar(pred_xpt, OFFSET), get_H_image(pred_xpt, w2c, c2p)))
-
-
-def _single_predict(prev_xpt: np.ndarray, prev_cov: np.ndarray, A, Q=_Q_DEFAULT):
-    xpt = np.matmul(A, prev_xpt[0:4])
-    cov = np.matmul(A, np.matmul(prev_cov, A.T)) + Q
-    return xpt, cov
-
-
-def _single_update(pred_xpt: np.ndarray, pred_cov: np.ndarray, z: np.ndarray, H, R):
-    K = np.matmul(pred_cov, np.matmul(H.T, np.linalg.inv(np.matmul(H, np.matmul(pred_cov, H.T)) + R)))
-    xpt = pred_xpt + np.matmul(K, (z - np.matmul(H, pred_xpt)))
-    cov = pred_cov - np.matmul(np.matmul(K, H), pred_cov)
-    return xpt, cov
-
-
-def _single_radar_comparison(pred_xpt: np.ndarray, z: np.ndarray, offset: np.ndarray):
-    x, y, vx, vy = pred_xpt[0] - offset[0], pred_xpt[1] - offset[1], pred_xpt[2], pred_xpt[3]
-    r = np.sqrt(np.square(x) + np.square(y))
-    if x == 0:
-        theta = 0
-    elif y == 0 and x < 0:
-        theta = 90
-    elif y == 0 and x > 0:
-        theta = -90
-    else:
-        theta = -np.arctan(x / y)
-        if x < 0 and y < 0:
-            theta = 180 - theta
-        elif x > 0 and y < 0:
-            theta = -180 + theta
-    vn = (vy * y + vx * x) / r
-    zp = np.array([r, theta, vn])
-    print("\033[0;31mCMP, Radar:\033[0m", z, zp)
-    d = np.linalg.norm(z - zp)
-    return d
-
-
-def _single_image_comparison(pred_xpt: np.ndarray, z: np.ndarray, w2c, c2p):
-    pos = np.array([pred_xpt[0], pred_xpt[1], HEIGHT, 1])
-    zp, _ = w2p(pos, w2c, c2p)
-    zp = zp[0:2]
-    print("\033[0;31mCMP, Image:\033[0m", z, zp)
-    d = np.linalg.norm(z - zp)
-    return d
-
-
-def _single_match_comparison(pred_xpt: np.ndarray, z: np.ndarray, offset: np.ndarray, w2c, c2p):
-    d1 = _single_radar_comparison(pred_xpt, z[0:3], offset)
-    d2 = _single_image_comparison(pred_xpt, z[3:5], w2c, c2p)
-    d = d1 * _WEIGHT_RADAR + d2 * _WEIGHT_IMAGE
-    return d
-
-
-def bulk_predict(prev_xpt_pile: list, prev_cov_pile: list, A, Q=_Q_DEFAULT):
-    p0 = list(map(lambda x, c: _single_predict(x, c, A, Q), prev_xpt_pile, prev_cov_pile))
-    return [xpt for xpt, _ in p0], [cov for _, cov in p0]
-
-
-def bulk_comparison(pred_xpt_pile, zms, zrs, zis, w2c, c2p, offset=OFFSET):
-    # row -> xpts, col -> zs
-    xpt_num = len(pred_xpt_pile)
-    z_num = len(zms) + len(zrs) + len(zis)
-    cmp_match = np.array([_single_match_comparison(x, z, offset, w2c, c2p) for x in pred_xpt_pile for z in zms]).reshape(xpt_num, len(zms))
-    cmp_radar = np.array([_single_radar_comparison(x, z, offset) for x in pred_xpt_pile for z in zrs]).reshape(xpt_num, len(zrs))
-    cmp_image = np.array([_single_image_comparison(x, z, w2c, c2p) for x in pred_xpt_pile for z in zis]).reshape(xpt_num, len(zis))
-    cmp = np.concatenate((cmp_match, cmp_radar, cmp_image), axis=1)
-    print("Bulk Comparison:", cmp)
-    row_idx, col_idx = linear_sum_assignment(cmp)
-    # threshold filter
-    fil = cmp[row_idx, col_idx] < _THRESHOLD
-    row_idx = np.extract(fil, row_idx)
-    lost_idx = np.setdiff1d(np.arange(xpt_num), row_idx)
-    col_idx = np.extract(fil, col_idx)
-    new_idx = np.setdiff1d(np.arange(z_num), col_idx)
-    # existing objs, lost objs, new objs
-    return (row_idx, col_idx), lost_idx, new_idx
-
-
-def bulk_update(exist_idx, state_xpt_pile, state_cov_pile, pred_xpt_pile, pred_cov_pile, zms, zrs, zis, w2c, c2p, offset=OFFSET):
-    for pred_idx, z_idx in zip(exist_idx[0], exist_idx[1]):
-        if z_idx < len(zms):
-            # match
-            H = get_H_match(pred_xpt_pile[pred_idx], w2c, c2p)
-            z = zms[z_idx]
-            R = 0.1 * np.eye(5)
-        elif z_idx < len(zms) + len(zrs):
-            # radar
-            H = get_H_radar(pred_xpt_pile[pred_idx], offset)
-            z = zrs[z_idx - len(zms)]
-            R = 0.1 * np.eye(3)
+    def __h_radar(self, xpt: np.ndarray):
+        x, y, vx, vy = xpt[0] - self.radar_pos[0], xpt[1] - self.radar_pos[1], xpt[2], xpt[3]
+        r = np.sqrt(np.square(x) + np.square(y))
+        if x == 0:
+            theta = 0
+        elif y == 0 and x < 0:
+            theta = 90
+        elif y == 0 and x > 0:
+            theta = -90
         else:
-            # image
-            H = get_H_image(pred_xpt_pile[pred_idx], w2c, c2p)
-            z = zis[z_idx - len(zms) - len(zrs)]
-            R = 0.1 * np.eye(2)
-        xpt, cov = _single_update(pred_xpt_pile[pred_idx], pred_cov_pile[pred_idx], z, H, R)
-        age = state_xpt_pile[pred_idx][4] + 1
-        if age > 3:
-            age = 3
-        state_xpt_pile[pred_idx] = np.concatenate((xpt, [age, state_xpt_pile[pred_idx][5]])) 
-        state_cov_pile[pred_idx] = cov
+            theta = -np.arctan(x / y)
+            if x < 0 and y < 0:
+                theta = 180 - theta
+            elif x > 0 and y < 0:
+                theta = -180 + theta
+        vn = (vy * y + vx * x) / r
+        zp = np.array([r, theta, vn])
+        return zp
 
+    def __h_image(self, xpt: np.ndarray):
+        pos = np.array([xpt[0], xpt[1], self.radar_pos[2], 1])
+        zp, _ = w2p(pos, self.w2c, self.c2p)
+        return zp[0:2]
 
-def bulk_lost(lost_idx, state_xpt_pile, state_cov_pile):
-    for i in lost_idx:
-        state_xpt_pile[i][4] -= 1
-    i, m = 0, len(state_xpt_pile)
-    while(i < m):
-        if state_xpt_pile[i][4] < 0:
-            state_xpt_pile.pop(i)
-            state_cov_pile.pop(i)
-            m = m - 1
-        else:
-            i = i + 1
+    def __single_predict(self, prev_xpt: np.ndarray, prev_cov: np.ndarray, A, Q):
+        xpt = np.matmul(A, prev_xpt[0:4])
+        cov = np.matmul(A, np.matmul(prev_cov, A.T)) + Q
+        return xpt, cov
+    
+    def __single_update(self, pred_xpt: np.ndarray, pred_cov: np.ndarray, z: np.ndarray, h, H, R):
+        K = np.matmul(pred_cov, np.matmul(H.T, np.linalg.inv(np.matmul(H, np.matmul(pred_cov, H.T)) + R)))
+        xpt = pred_xpt + np.matmul(K, (z - h(pred_xpt)))
+        cov = pred_cov - np.matmul(np.matmul(K, H), pred_cov)
+        return xpt, cov
 
+    def __single_comparison(self, pred_xpt: np.ndarray, z: np.ndarray, h):
+        pred_z = h(pred_xpt)
+        d = np.linalg.norm(z - pred_z)
+        print("\033[1;31mCMP (z/x2z):\033[0m", z, pred_z)
+        return d
 
-def bulk_new(new_idx, state_xpt_pile, state_cov_pile, zms, zrs, zis, max_id):
-    for i in new_idx:
-        if i < len(zms):
-            # match
-            x = np.array([-1.3, -1.5, 0, 0, 0, max_id])
-        elif i < len(zms) + len(zrs):
-            # radar
-            x = np.array([-1.3, -1.5, 0, 0, 0, max_id])
-        else:
-            # image
-            x = np.array([-1.3, -1.5, 0, 0, 0, max_id])
-        state_xpt_pile.append(x)
-        state_cov_pile.append(np.full((4, 4), 1e6))
-        max_id += 1
-    return max_id
+    def __single_match_comparison(self, pred_xpt: np.ndarray, z: np.ndarray):
+        d1 = self.__single_comparison(pred_xpt, z[0:3], self.__h_radar)
+        d2 = self.__single_comparison(pred_xpt, z[3:5], self.__h_image)
+        d = d1 * self.WEIGHT_RADAR + d2 * self.WEIGHT_IMAGE
+        return d
 
+    def bulk_predict(self, A, Q):
+        p0 = list(map(lambda x, c: self.__single_predict(x, c, A, Q), self.xpt_pile, self.cov_pile))
+        return [xpt for xpt, _ in p0], [cov for _, cov in p0]
 
-def bulk_output():
-    pass
+    def bulk_comparison(self, zms: np.ndarray, zrs: np.ndarray, zis: np.ndarray):
+        # row -> xpts, col -> zs
+        xpt_num = len(self.pred_xpt_pile)
+        z_num = len(zms) + len(zrs) + len(zis)
+        cmp_match = np.array([self.__single_match_comparison(x, z) for x in self.pred_xpt_pile for z in zms]).reshape(xpt_num, len(zms))
+        cmp_radar = np.array([self.__single_comparison(x, z, self.__h_radar) for x in self.pred_xpt_pile for z in zrs]).reshape(xpt_num, len(zrs))
+        cmp_image = np.array([self.__single_comparison(x, z, self.__h_image) for x in self.pred_xpt_pile for z in zis]).reshape(xpt_num, len(zis))
+        cmp = np.concatenate((cmp_match, cmp_radar, cmp_image), axis=1)
+        print("Bulk Comparison:", cmp)
+        row_idx, col_idx = linear_sum_assignment(cmp)
+        # threshold filter
+        fil = cmp[row_idx, col_idx] < self.THRESHOLD
+        row_idx = np.extract(fil, row_idx)
+        lost_idx = np.setdiff1d(np.arange(xpt_num), row_idx)
+        col_idx = np.extract(fil, col_idx)
+        new_idx = np.setdiff1d(np.arange(z_num), col_idx)
+        # existing objs, lost objs, new objs
+        return (row_idx, col_idx), lost_idx, new_idx
+
+    def bulk_update(self, exist_idx: np.ndarray, zms: np.ndarray, zrs: np.ndarray, zis: np.ndarray):
+        for pred_idx, z_idx in zip(exist_idx[0], exist_idx[1]):
+            if z_idx < len(zms):
+                # match
+                H = self.__H_match(self.pred_xpt_pile[pred_idx])
+                z = zms[z_idx]
+                R = 0.1 * np.eye(5)
+            elif z_idx < len(zms) + len(zrs):
+                # radar
+                H = self.__H_radar(self.pred_xpt_pile[pred_idx])
+                z = zrs[z_idx - len(zms)]
+                R = 0.1 * np.eye(3)
+            else:
+                # image
+                H = self.__H_image(self.pred_xpt_pile[pred_idx])
+                z = zis[z_idx - len(zms) - len(zrs)]
+                R = 0.1 * np.eye(2)
+            xpt, cov = self.__single_update(self.pred_xpt_pile[pred_idx], self.pred_cov_pile[pred_idx], z, H, R)
+            age = self.xpt_pile[pred_idx][4] + 1
+            if age > self.MAX_AGE:
+                age = self.MAX_AGE
+            self.xpt_pile[pred_idx] = np.concatenate((xpt, [age, self.xpt_pile[pred_idx][5]])) 
+            self.cov_pile[pred_idx] = cov
+
+    def bulk_lost(self, lost_idx: np.ndarray):
+        for i in lost_idx:
+            self.xpt_pile[i][4] -= 1
+        i, m = 0, len(self.xpt_pile)
+        while(i < m):
+            if self.xpt_pile[i][4] < 0:
+                self.xpt_pile.pop(i)
+                self.cov_pile.pop(i)
+                m = m - 1
+            else:
+                i = i + 1
+
+    def bulk_new(self, new_idx: np.ndarray, zms: np.ndarray, zrs: np.ndarray, zis: np.ndarray):
+        for i in new_idx:
+            if i < len(zms):
+                # match
+                x = np.array([-1.3, -1.8, 0, 0, 0, self.max_id])
+            elif i < len(zms) + len(zrs):
+                # radar
+                zr = zrs[i - len(zms)]
+                xw = self.radar_pos[0] - zr[0] * np.sin(zr[1] * np.pi / 180)
+                yw = self.radar_pos[1] + zr[1] * np.cos(zr[1] * np.pi / 180)
+                x = np.array([xw, yw, 0, 0, 0, self.max_id])
+            else:
+                # image
+                x = np.array([-1.3, -1.8, 0, 0, 0, self.max_id])
+            self.xpt_pile.append(x)
+            self.cov_pile.append(np.full((4, 4), 1e6))
+            self.max_id += 1
+
+    def output():
+        pass
+
+    def all_in_one(self, A: np.ndarray, Q: np.ndarray, zms: np.ndarray, zrs: np.ndarray, zis: np.ndarray):
+        self.bulk_predict(A, Q)
+        exist_idx, lost_idx, new_idx = self.bulk_comparison(zms, zrs, zis)
+        self.bulk_update(exist_idx, zms, zrs, zis)
+        self.bulk_lost(lost_idx)
+        self.bulk_new(new_idx, zms, zrs, zis)
