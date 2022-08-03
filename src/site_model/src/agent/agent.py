bafb1c76ad68d60044afbdf06f1db4faaa778744
YangXiuyu <gzzyyxy@gmail.com>
2022-08-03 18:05:15 +0800

    update agent: add set_lane

--- src/site_model/src/agent/agent.py
+++ src/site_model/src/agent/agent.py
@@ -1,8 +1,3 @@
-#############################################################
-#   This py file acts as agent representing the overpass.   #
-#############################################################
-
-
 #!/usr/bin/python3
 import numpy as np
 import random
@@ -17,7 +12,11 @@ class Agent():
         for i in range(6):
             lane = np.loadtxt(lanes_path+"/"+str(i)+".txt", dtype=np.float64)
             self.lanes.append(lane)
-        print(self.lanes)
+        self.nodes = [[],
+                      [],
+                      [],
+                      []]
+        self.lane = -1
     
     
     def is_front(self, vehicle: np.array, point: np.array, alpha):
@@ -27,60 +26,60 @@ class Agent():
         return abs(np.arctan2(point[1]-vehicle[1], point[0]-vehicle[0])-alpha) <= np.pi*2/3
 
 
+    def is_node(self, vehicle: np.array):
+        pass
+
+
     def get_mindist(self, dists):
         min_dist = np.inf
         lane: int = -1
         for i, dist in enumerate(dists):
-            if np.min(dist) < min_dist:
+            if np.min(dist) < min_dist and np.min(dist) >= self.obj_threshold:
                 min_dist = np.min(dist)
                 lane = i
         if lane == -1:
-            return lane, -1, -1
+            return lane, -1
         loc = np.where(dists[lane]==min_dist)[0][0]
-        return lane, loc, min_dist
-                    
+        return lane, loc
+
+
+    def set_lane(self, vehicle: np.array, alpha):
+        if self.lane == -1:                                     # no current lane
+            dists = [np.array([np.linalg.norm(point - vehicle) if self.is_front(vehicle, point, alpha) else np.inf for point in lane]) for lane in self.lanes]
+        elif self.is_node(vehicle):                             # if at nodes
+            # need to make decision
+            return lane, -2
+        else:                                                   # not at nodes
+            dists = [np.array([np.linalg.norm(point - vehicle) if self.is_front(vehicle, point, alpha) else np.inf for point in self.lanes[self.lane]])]
+        lane, loc = self.get_mindist(dists)
+        return lane, loc
+
 
     def set_obj(self, vehicle: np.array, alpha):
         """
-            set objective & if need to make decision
+            set objective
         """
-        dists = [np.array([np.linalg.norm(point - vehicle) if self.is_front(vehicle, point, alpha) else np.inf for point in lane]) for lane in self.lanes]
-        lane, loc, min_dist = self.get_mindist(dists)
-        if lane == -1:                                          # no candidate objectives in front of vehicle
+        lane, loc = self.set_lane(vehicle, alpha)
+        if loc == -1:                                           # no candidate objectives in front of vehicle
             return []
-        while min_dist < self.obj_threshold:                    # remove the nearer points
-            print("lane:", lane, "loc:", loc)
-            dists[lane][loc] = np.inf
-            lane, loc, min_dist = self.get_mindist(dists)
-            if lane == -1:
-                return []
+        if loc == -2:
+            self.lane = lane                                    # switch lane
 
-        obj_loc = [lane, loc]
+        obj_loc = [self.lane, loc]
         obj = self.lanes[obj_loc[0]][obj_loc[1]]
-
-        candi_obj = [obj]
-        for i, dist in enumerate(dists):                        # whether to make decision
-            if obj_loc[0] == i:
-                continue
-            if abs(np.min(dist)-min_dist) <= self.multi_threshold:
-                candit_obj_loc = np.where(dists[i]==np.min(dist))[0][0]
-                candi_obj.append(self.lanes[i][candit_obj_loc])
-
-        return candi_obj
+        return [obj]
         
 
     def set_control(self, vehicle: np.array, alpha, msgradcam: MsgRadCam, msglidcam: MsgLidCam):
+        """
+            return the speed and angle to the vehicle
+        """
         candi_obj = self.set_obj(vehicle, alpha)
-        if len(candi_obj) == 0:
-            return 0, -1                                        # no candidate objectives: fall back
-        elif len(candi_obj) != 1:                               # need to make decision
-            print("need to make decision.")
-            rand_obj = random.randint(0, len(candi_obj)-1)                          # randomly choose
-            obj = candi_obj[rand_obj]
-        elif len(candi_obj) == 1:                                                   # only one objective
+        if len(candi_obj) == 0:                                 # no candidate objectives: fall back
+            return 0, -1
+        else:                                                   # only one objective
             obj = candi_obj[0]
         print("obj:", obj)
         diff = np.arctan2(obj[1]-vehicle[1], obj[0]-vehicle[0]) - alpha
         turn = diff / (np.pi/4) if abs(diff) <= np.pi/4 else np.sign(diff)          # left: turn=1; right: turn = -1
-        print("turn:", turn)
         return turn, 1                                                              # return angle, speed
