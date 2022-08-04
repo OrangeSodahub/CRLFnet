4ec22dbcba2fa452d546c35c32499809ed4f8e30
YangXiuyu <gzzyyxy@gmail.com>
2022-08-04 22:24:31 +0800

    add other 3 base links

--- src/site_model/src/agent/dispatch.py
+++ src/site_model/src/agent/dispatch.py
@@ -2,6 +2,7 @@
 
 import argparse
 from pathlib import Path
+from typing import Tuple
 import numpy as np
 import yaml
 
@@ -19,93 +20,52 @@ from .agent import Agents
 from .scene import SceneMap
 
 
-def set_throttle_steer(odom1: Odometry, odom2: Odometry, odom3: Odometry, odom4: Odometry, msgradcam: MsgRadCam = None, msglidcam: MsgLidCam = None):
+class PublisherBundle:
 
-    # declear the publish tools
-    # publish the vis data
-    pub_data = rospy.Publisher('/data', Float64, queue_size=1)
-
-    pub_vel_left_rear_wheel_1 = rospy.Publisher('/deepracer1/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel_1 = rospy.Publisher('/deepracer1/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel_1 = rospy.Publisher('/deepracer1/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel_1 = rospy.Publisher('/deepracer1/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_pos_left_steering_hinge_1 = rospy.Publisher('/deepracer1/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge_1 = rospy.Publisher('/deepracer1/right_steering_hinge_position_controller/command', Float64, queue_size=1)
-
-    pub_vel_left_rear_wheel_2 = rospy.Publisher('/deepracer2/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel_2 = rospy.Publisher('/deepracer2/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel_2 = rospy.Publisher('/deepracer2/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel_2 = rospy.Publisher('/deepracer2/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_pos_left_steering_hinge_2 = rospy.Publisher('/deepracer2/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge_2 = rospy.Publisher('/deepracer2/right_steering_hinge_position_controller/command', Float64, queue_size=1)
-
-    pub_vel_left_rear_wheel_3 = rospy.Publisher('/deepracer3/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel_3 = rospy.Publisher('/deepracer3/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel_3 = rospy.Publisher('/deepracer3/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel_3 = rospy.Publisher('/deepracer3/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_pos_left_steering_hinge_3 = rospy.Publisher('/deepracer3/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge_3 = rospy.Publisher('/deepracer3/right_steering_hinge_position_controller/command', Float64, queue_size=1)
-
-    pub_vel_left_rear_wheel_4 = rospy.Publisher('/deepracer4/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel_4 = rospy.Publisher('/deepracer4/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel_4 = rospy.Publisher('/deepracer4/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel_4 = rospy.Publisher('/deepracer4/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_pos_left_steering_hinge_4 = rospy.Publisher('/deepracer4/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge_4 = rospy.Publisher('/deepracer4/right_steering_hinge_position_controller/command', Float64, queue_size=1)
-
-    # publish the decisions
-    # get all status
-    status = []
-    status.append(get_status(odom1))
-    status.append(get_status(odom2))
-    status.append(get_status(odom3))
-    status.append(get_status(odom4))
-    steers, throttles = agents.navigate(status, msgradcam, msglidcam)
-    throttles = [throttle*10 for throttle in throttles]
-
-    pub_vel_left_rear_wheel_1.publish(throttles[0])
-    pub_vel_right_rear_wheel_1.publish(throttles[0])
-    pub_vel_left_front_wheel_1.publish(throttles[0])
-    pub_vel_right_front_wheel_1.publish(throttles[0])
-    pub_pos_left_steering_hinge_1.publish(steers[0])
-    pub_pos_right_steering_hinge_1.publish(steers[0])
-
-    # vehicle2
-    pub_vel_left_rear_wheel_2.publish(throttles[1])
-    pub_vel_right_rear_wheel_2.publish(throttles[1])
-    pub_vel_left_front_wheel_2.publish(throttles[1])
-    pub_vel_right_front_wheel_2.publish(throttles[1])
-    pub_pos_left_steering_hinge_2.publish(steers[1])
-    pub_pos_right_steering_hinge_2.publish(steers[1])
-
-    # vehicle3
-    pub_vel_left_rear_wheel_3.publish(throttles[2])
-    pub_vel_right_rear_wheel_3.publish(throttles[2])
-    pub_vel_left_front_wheel_3.publish(throttles[2])
-    pub_vel_right_front_wheel_3.publish(throttles[2])
-    pub_pos_left_steering_hinge_3.publish(steers[2])
-    pub_pos_right_steering_hinge_3.publish(steers[2])
-
-    # vehicle4
-    pub_vel_left_rear_wheel_4.publish(throttles[3])
-    pub_vel_right_rear_wheel_4.publish(throttles[3])
-    pub_vel_left_front_wheel_4.publish(throttles[3])
-    pub_vel_right_front_wheel_4.publish(throttles[3])
-    pub_pos_left_steering_hinge_4.publish(steers[3])
-    pub_pos_right_steering_hinge_4.publish(steers[3])
-
-    # data
-    pub_data.publish(np.random()*10)
+    def __init__(self, vehicle_name: str) -> None:
+        # l: left, r: right, f: front, r: rear
+        self.lr_wheel = rospy.Publisher('/{}/left_rear_wheel_velocity_controller/command'.format(vehicle_name), Float64, queue_size=1)
+        self.rr_wheel = rospy.Publisher('/{}/right_rear_wheel_velocity_controller/command'.format(vehicle_name), Float64, queue_size=1)
+        self.lf_wheel = rospy.Publisher('/{}/left_front_wheel_velocity_controller/command'.format(vehicle_name), Float64, queue_size=1)
+        self.rf_wheel = rospy.Publisher('/{}/right_front_wheel_velocity_controller/command'.format(vehicle_name), Float64, queue_size=1)
+        self.l_steering_hinge = rospy.Publisher('/{}/left_steering_hinge_position_controller/command'.format(vehicle_name), Float64, queue_size=1)
+        self.r_steering_hinge = rospy.Publisher('/{}/right_steering_hinge_position_controller/command'.format(vehicle_name), Float64, queue_size=1)
     
+    def publish(self, throttle: float, steer: float) -> None:
+        self.lr_wheel.publish(throttle)
+        self.rr_wheel.publish(throttle)
+        self.lf_wheel.publish(throttle)
+        self.rf_wheel.publish(throttle)
+        self.l_steering_hinge.publish(steer)
+        self.r_steering_hinge.publish(steer)
+
+
+def odom2pose(odom: Odometry) -> Tuple[np.ndarray, float]:
+    pos = odom.pose.pose.position
+    orient = odom.pose.pose.orientation
+    r, p, y = euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
+    return np.array([pos.x, pos.y]), y
+
+
+def set_control(odom1: Odometry, odom2: Odometry, odom3: Odometry, odom4: Odometry,
+                msgradcam: MsgRadCam = None, msglidcam: MsgLidCam = None
+               ) -> None:
+    global pub_data
+    global pub_1, pub_2, pub_3, pub_4
+
+    poses = [odom2pose(odom1), odom2pose(odom2), odom2pose(odom3), odom2pose(odom4)]
+    steers, throttles = agents.navigate(poses, msgradcam, msglidcam)
+    throttles = [t * 10 for t in throttles]
+
+    pub_1.publish(throttles[0], steers[0])
+    pub_2.publish(throttles[1], steers[1])
+    pub_3.publish(throttles[2], steers[2])
+    pub_4.publish(throttles[3], steers[3])
     
-def get_status(odom: Odometry):
-    vehicle = np.array([odom.pose.pose.position.x, odom.pose.pose.position.y])
-    r, p, y = euler_from_quaternion([odom.pose.pose.orientation.x, odom.pose.pose.orientation.y,
-                                    odom.pose.pose.orientation.z, odom.pose.pose.orientation.w])
-    return [vehicle, y]
+    pub_data.publish(np.random.random()*10)
 
 
-def servo_commands():
+def servo_commands() -> None:
 
     rospy.init_node('servo_commands', anonymous=True)
 
@@ -119,19 +79,26 @@ def servo_commands():
     sub_odom3 = message_filters.Subscriber('/deepracer3/base_pose_ground_truth', Odometry)
     sub_odom4 = message_filters.Subscriber('/deepracer4/base_pose_ground_truth', Odometry)
     sync = message_filters.ApproximateTimeSynchronizer([sub_odom1, sub_odom2, sub_odom3, sub_odom4], 1, 1)
-    sync.registerCallback(set_throttle_steer)
+    sync.registerCallback(set_control)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-if __name__ == '__main__':
-    # get ROOT DIR
-    ROOT_DIR = Path(__file__).resolve().parents[2]
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", help="path to config file", metavar="FILE", required=False, default= str(ROOT_DIR / 'config/config.yaml'))
-    parser.add_argument("--vis", help="whether to visualize", action='store_true', required=False)
-    params = parser.parse_args()
 
+if __name__ == '__main__':
+    ROOT_DIR = Path(__file__).resolve().parents[2]
+    CONFIG_FILE = ROOT_DIR.joinpath('config/config.yaml')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help     = "path to config file",
+                                    metavar  = "FILE",
+                                    required = False,
+                                    default  = str(CONFIG_FILE)
+                       )
+    parser.add_argument("--vis",    help     = "whether to visualize",
+                                    action   = 'store_true',
+                                    required = False
+                       )
+    params = parser.parse_args()
     with open(params.config, 'r') as f:
         try:
             config = yaml.load(f, Loader=yaml.FullLoader)
@@ -141,5 +108,12 @@ if __name__ == '__main__':
 
     MAP_DIR = ROOT_DIR.joinpath(config['dispatch']['scene_map'])
     scene_map = SceneMap(MAP_DIR)
-    agents = Agents(ROOT_DIR / config['lanes']['file_dir'], scene_map, 4)
+    agents = Agents(scene_map, 4)
+
+    pub_data = rospy.Publisher('/data', Float64, queue_size=1)
+    pub_1 = PublisherBundle('deepracer1')
+    pub_2 = PublisherBundle('deepracer2')
+    pub_3 = PublisherBundle('deepracer3')
+    pub_4 = PublisherBundle('deepracer4')
+
     servo_commands()
