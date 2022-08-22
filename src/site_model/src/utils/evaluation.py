99a53cf8ee04c8ad288b3dabe06476200e2eba4a
YangXiuyu <gzzyyxy@gmail.com>
2022-08-22 10:44:58 +0800

    update eval3d

--- src/site_model/src/utils/evaluation.py
+++ src/site_model/src/utils/evaluation.py
@@ -14,15 +14,12 @@ class eval3d():
         self.writter = SummaryWriter(log_dir=log_dir)
         self.heat_map_pose = np.zeros((32, 64))
         self.heat_map_alpha = np.zeros((32, 64))
+        self.heat_map_alpha_similarity = np.zeros((32, 64))
         self.heat_map_iou = np.zeros((32, 64))
         self.heat_map_count = np.zeros((32, 64))
 
         self.counter = 0
         self.global_counter = 0
-        self.alpha_diff = 0
-        self.pose_diff = 0
-        self.iou3d = 0
-        self.iou_bev = 0
 
         N_SAMPLE_PTS = 41
         self.thresholds = np.linspace(0.0, 1.0, num=N_SAMPLE_PTS, endpoint=True)
@@ -41,42 +38,37 @@ class eval3d():
             gt_boxes3d = common_utils.get_gt_boxes3d(odom)
             
             # rotation
-            alpha_precision, alpha_cur_precision = self.eval_rotation(gt_boxes3d, preboxes3d)
-            self.writter.add_scalars('alpha_precision', {'pre': alpha_precision}, self.counter)
-            self.writter.add_scalars('alpha_cur_precision', {'pre': alpha_cur_precision}, self.counter)
-            alpha_precision, alpha_cur_precision = self.eval_rotation(gt_boxes3d, postboxes3d)
-            self.writter.add_scalars('alpha_precision', {'post': alpha_precision}, self.counter)
-            self.writter.add_scalars('alpha_cur_precision', {'post': alpha_cur_precision}, self.counter)
+            alpha_precision_pre, alpha_similarity_pre = self.eval_rotation(gt_boxes3d, preboxes3d)
+            self.writter.add_scalars('alpha_precision', {'pre': alpha_precision_pre}, self.counter)
+            alpha_precision_post, alpha_similarity_post = self.eval_rotation(gt_boxes3d, postboxes3d)
+            self.writter.add_scalars('alpha_precision', {'post': alpha_precision_post}, self.counter)
 
             # pose
-            pose_mean_diff, pose_cur_diff = self.eval_pose(gt_boxes3d, preboxes3d)
-            self.writter.add_scalars('pose_mean_diff', {'pre': pose_mean_diff}, self.counter)
-            self.writter.add_scalars('pose_cur_diff', {'pre': pose_cur_diff}, self.counter)
-            pose_mean_diff, pose_cur_diff = self.eval_pose(gt_boxes3d, postboxes3d)
-            self.writter.add_scalars('pose_mean_diff', {'post': pose_mean_diff}, self.counter)
-            self.writter.add_scalars('pose_cur_diff', {'post': pose_cur_diff}, self.counter)
+            pose_diff = self.eval_pose(gt_boxes3d, preboxes3d)
+            self.writter.add_scalars('pose_diff', {'pre': pose_diff}, self.counter)
+            pose_diff = self.eval_pose(gt_boxes3d, postboxes3d)
+            self.writter.add_scalars('pose_diff', {'post': pose_diff}, self.counter)
 
             # iou and tp
-            iou_bev_mean, iou_bev_cur = self.eval_iou(gt_boxes3d, preboxes3d)
-            self.writter.add_scalars('iou_bev_mean', {'pre': iou_bev_mean}, self.counter)
-            self.writter.add_scalars('iou_bev_cur', {'pre': iou_bev_cur}, self.counter)
-            iou_bev_mean, iou_bev_cur = self.eval_iou(gt_boxes3d, postboxes3d)
-            self.writter.add_scalars('iou_bev_mean', {'post': iou_bev_mean}, self.counter)
-            self.writter.add_scalars('iou_bev_cur', {'post': iou_bev_cur}, self.counter)
+            iou_bev_pre = self.eval_iou(gt_boxes3d, preboxes3d)
+            self.writter.add_scalars('iou_bev', {'pre': iou_bev_pre}, self.counter)
+            iou_bev_post = self.eval_iou(gt_boxes3d, postboxes3d)
+            self.writter.add_scalars('iou_bev', {'post': iou_bev_post}, self.counter)
 
             # score
-            self.writter.add_scalars('alpha_cur_precision', {'score': scores3d[0]}, self.counter)
-            self.writter.add_scalars('pose_cur_diff', {'score': 1-scores3d[0]}, self.counter)
-            self.writter.add_scalars('iou_bev_cur', {'score': scores3d[0]}, self.counter)
+            self.writter.add_scalars('alpha_precision', {'score': scores3d[0]}, self.counter)
+            self.writter.add_scalars('pose_diff', {'score': 1-scores3d[0]}, self.counter)
+            self.writter.add_scalars('iou_bev', {'score': scores3d[0]}, self.counter)
 
             # heat map
             if isinstance(odom, np.ndarray):
                 i = int((odom[0] + 2) // 0.125)
                 j = int((odom[1] + 5) // 0.125)
                 if i>=0 and i<32 and j>=0 and j<64:
-                    self.heat_map_pose[i][j] += pose_cur_diff
-                    self.heat_map_alpha[i][j] += alpha_cur_precision
-                    self.heat_map_iou[i][j] += iou_bev_cur
+                    self.heat_map_pose[i][j] += pose_diff
+                    self.heat_map_alpha[i][j] += alpha_precision_pre
+                    self.heat_map_alpha_similarity[i][j] += alpha_similarity_pre
+                    self.heat_map_iou[i][j] += iou_bev_pre
                     self.heat_map_count[i][j] += 1
         else:
             self.tp_fp_fn[2] += 1
@@ -88,30 +80,29 @@ class eval3d():
                     if self.heat_map_count[i][j] != 0:
                         self.heat_map_pose[i][j] /= self.heat_map_count[i][j]
                         self.heat_map_alpha[i][j] /= self.heat_map_count[i][j]
+                        self.heat_map_alpha_similarity[i][j] /= self.heat_map_count[i][j]
                         self.heat_map_iou[i][j] /= self.heat_map_count[i][j]
             np.savetxt(os.path.join(self.log_dir, 'heat_map_pose.txt'), self.heat_map_pose)
             np.savetxt(os.path.join(self.log_dir, 'heat_map_alpha.txt'), self.heat_map_alpha)
+            np.savetxt(os.path.join(self.log_dir, 'heat_map_alpha_similarity.txt'), self.heat_map_alpha_similarity)
             np.savetxt(os.path.join(self.log_dir, 'heat_map_iou_bev.txt'), self.heat_map_iou)
             np.savetxt(os.path.join(self.log_dir, 'heat_map_iou_count.txt'), self.heat_map_count)
             
 
     def eval_rotation(self, gt_boxes3d, boxes3d):
         # ((boxes3d[0][6] - np.pi) if boxes3d[0][6] >= 0 else (np.pi + boxes3d[0][6]))
-        alpha_cur_diff = np.abs(boxes3d[0][6] - gt_boxes3d[0][6]) # pred_boxes3d[0] -> for one car
-        self.alpha_diff += alpha_cur_diff
-        alpha_cur_precision = 1 - (alpha_cur_diff) / (2*np.pi)
-        alpha_precision = 1 - (self.alpha_diff / self.counter) / (2*np.pi)
-        return alpha_precision, alpha_cur_precision
+        alpha_diff = np.abs(boxes3d[0][6] - gt_boxes3d[0][6]) # pred_boxes3d[0] -> for one car
+        alpha_precision = 1 - (alpha_diff) / (2*np.pi)
+        alpha_similarity = (1 + np.cos(2*alpha_diff)) / 2
+        return alpha_precision, alpha_similarity
 
 
     def eval_pose(self, gt_boxes3d, boxes3d):
         # pose_x_y
-        x_cur_diff = np.abs(gt_boxes3d[0][0]-boxes3d[0][0])
-        y_cur_diff = np.abs(gt_boxes3d[0][1]-boxes3d[0][1])
-        pose_cur_diff = np.sqrt(np.square(x_cur_diff)+np.square(y_cur_diff))
-        self.pose_diff += pose_cur_diff
-        pose_mean_diff = self.pose_diff / self.counter
-        return pose_mean_diff, pose_cur_diff
+        x_diff = np.abs(gt_boxes3d[0][0]-boxes3d[0][0])
+        y_diff = np.abs(gt_boxes3d[0][1]-boxes3d[0][1])
+        pose_diff = np.sqrt(np.square(x_diff)+np.square(y_diff))
+        return pose_diff
 
 
     def eval_iou(self, gt_boxes3d, boxes3d):
@@ -119,26 +110,17 @@ class eval3d():
         gt_boxes3d_gpu = torch.tensor(gt_boxes3d, dtype=torch.float32).cuda()                           # load to gpu: double->float64 float->float32
         boxes3d_gpu = torch.tensor(boxes3d, dtype=torch.float32).cuda()                                 # load to gpu
 
-        iou3d_cur = boxes_iou3d_gpu(boxes_a=gt_boxes3d_gpu, boxes_b=boxes3d_gpu)
-        iou_bev_cur = boxes_iou_bev_gpu(boxes_a=gt_boxes3d_gpu, boxes_b=boxes3d_gpu)
-        iou3d_cur_cpu = iou3d_cur.cpu().numpy()                                                         # convert tensor to numpy
-        iou_bev_cur_cpu = iou_bev_cur.cpu().numpy()
-        # self.logger.log_value('iou3d_cur', iou3d_cur_cpu[0][0], self.counter)                         # here [0][0] is for one car!!!
-        # self.logger.log_value('iou_bev_cur', iou_bev_cur_cpu[0][0], self.counter)
-
-        self.iou3d += iou3d_cur_cpu[0][0]
-        self.iou_bev += iou_bev_cur_cpu[0][0]
-        iou3d_mean = self.iou3d / self.counter
-        iou_bev_mean = self.iou_bev / self.counter
-        # self.logger.log_value('iou3d_mean', iou3d_mean, self.counter)
-        # self.logger.log_value('iou_bev_mean', iou_bev_mean, self.counter)
+        iou3d = boxes_iou3d_gpu(boxes_a=gt_boxes3d_gpu, boxes_b=boxes3d_gpu)
+        iou_bev = boxes_iou_bev_gpu(boxes_a=gt_boxes3d_gpu, boxes_b=boxes3d_gpu)
+        iou3d_cpu = iou3d.cpu().numpy()                                                                 # iou3d_cpu[0][0]
+        iou_bev_cpu = iou_bev.cpu().numpy()                                                             # iou_bev_cpu[0][0]
 
         # caculate tp_fp_fn
-        true_or_false = iou_bev_cur_cpu[0][0] > self.thresholds
+        true_or_false = iou_bev_cpu[0][0] > self.thresholds
         self.tp_fp_fn[0] += true_or_false                                                               # tp
         self.tp_fp_fn[2] += np.logical_not(true_or_false)                                               # fn
 
-        return iou_bev_mean, iou_bev_cur_cpu[0][0]
+        return iou_bev_cpu[0][0]
 
 
 def boxes_iou_bev_cpu(boxes_a, boxes_b):
@@ -214,30 +196,4 @@ def boxes_iou3d_gpu(boxes_a, boxes_b):
 
 
 def bbox_iou(boxes3d: np.array, boxes2d: np.array, critierion=-1):
-    # N = boxes.shape[0]
-    # K = query_boxes.shape[0]
-    # overlaps = np.zeros((N, K), dtype=boxes.dtype)
-    # for k in range(K):
-    #     qbox_area = ((query_boxes[k, 2] - query_boxes[k, 0]) *
-    #                  (query_boxes[k, 3] - query_boxes[k, 1]))
-    #     for n in range(N):
-    #         iw = (min(boxes[n, 2], query_boxes[k, 2]) -
-    #               max(boxes[n, 0], query_boxes[k, 0]))
-    #         if iw > 0:
-    #             ih = (min(boxes[n, 3], query_boxes[k, 3]) -
-    #                   max(boxes[n, 1], query_boxes[k, 1]))
-    #             if ih > 0:
-    #                 if criterion == -1:
-    #                     ua = (
-    #                         (boxes[n, 2] - boxes[n, 0]) *
-    #                         (boxes[n, 3] - boxes[n, 1]) + qbox_area - iw * ih)
-    #                 elif criterion == 0:
-    #                     ua = ((boxes[n, 2] - boxes[n, 0]) *
-    #                           (boxes[n, 3] - boxes[n, 1]))
-    #                 elif criterion == 1:
-    #                     ua = qbox_area
-    #                 else:
-    #                     ua = 1.0
-    #                 overlaps[n, k] = iw * ih / ua
-    # return overlaps
     pass
